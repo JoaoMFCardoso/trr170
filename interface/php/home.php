4b418589b34a5f2bc17be8ba7e3d893cb1e02dfd
João M. F. Cardoso <joao.cardoso.geral@gmail.com>
2021-02-23 20:27:26 +0000

    Created a basic version of the home page.

---
+++ interface/php/home.php
@@ -0,0 +1,57 @@
+<html lang="">
+
+    <head>
+        <title>TRR 170 Metrics</title>
+        <link rel="stylesheet" href="/trr170/interface/css/general.css" >
+    </head>
+
+    <body>
+        <nav class="navbar">
+            <div id="navbar-contents" class="container">
+                <a href="#" id="navbar-logo"><img src="/trr170/interface/img/logo.png"  alt="TRR 170-DB homepage"></a>
+                <ul id="navbar-menu">
+                    <li class="active"><a href="#general_metrics">General Metrics</a></li>
+                    <li ><a href="#dataverse">Dataverse</a></li>
+                    <li ><a href="#dataset">Dataset</a></li>
+                    <li><a href="#file">File</a></li>
+                    <li><a href="#user">User</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div id="content" class="container">
+            <div id="introduction">
+                <p>The TRR 170 Dataverse Metrics</p>
+            </div>
+            <div class="metrics">
+                <div id="categories">
+                    <p>Total Dataverses</p>
+                    <p>Total Datasets</p>
+                    <p>Total Files</p>
+                    <p>Total Users</p>
+                </div>
+                <div id="graph">
+                    <p>graph</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 small">
+                        <span>Copyright © 2021</span>
+                    </div>
+                    <div class="col-sm-4 text-right">
+                        <div class="fundedbylogo">
+                            <span>Funded by</span>
+                            <a href="https://fu-berlin.de/" title="Freie Universität Berlin" target="_blank" rel="noopener"><img src="/trr170/interface/img/fuberlin.png" width="172" height="67.5" onerror="this.src='/trr170/interface/img/fuberlin.png'" alt="Freie Universität Berlin logo"></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
